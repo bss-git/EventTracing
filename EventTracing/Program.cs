a2ebda26e00e0ec37f2ada401ceb4bc765fd93a1
bss-git <alex.rodriga@gmail.com>
2020-02-18 12:19:27 +1000

    initial

---
+++ EventTracing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventTracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
